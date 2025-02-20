25301f74184653f61945a9b09753aa4001e58538
Gabriel Adriano <gabrieladrianofx@gmail.com>
2025-02-19 23:50:36 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    
+    <title>Genenciador de Filmes - ab filmes</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
