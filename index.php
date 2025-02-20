062303fcd052c95248a25944952f625c1b9cc1a2
Gabriel Adriano <gabrieladrianofx@gmail.com>
2025-02-20 00:43:02 -0300

    Customizando tema e importando imagens ao projeto

--- index.php
+++ index.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Rajdhani:wght@400;700&family=Rammetto+One&display=swap" rel="stylesheet">
+
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'purple-base': '#892CCD',
+                        'purple-light': '#A85FDD',
+
+                        'gray-100': '#0F0F1A',
+                        'gray-200': '#131320',
+                        'gray-300': '#1A1B2D',
+                        'gray-400': '#45455F',
+                        'gray-500': '#7A7B9F',
+                        'gray-600': '#B5B6C9',
+                        'gray-700': '#E4E5EC',
+
+                        'error-base': '#D04048',
+                        'error-light': '#F77980',
+                    },
+                    fontFamily: {
+                        display: ['Rammetto One'],
+                        title: ['Rajdhani'],
+                        body: ['Nunito Sans']
+                    },
+                }
+            }
+        }
+    </script>
 
     <title>Genenciador de Filmes - ab filmes</title>
 </head>
+
 <body>
     <header>
         <nav></nav>
@@ -15,4 +49,5 @@
     <main>
     </main>
 </body>
+
 </html>
