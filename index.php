b2597762873b3c42a9c1c7370563e783433a55c6
Gabriel Adriano <gabrieladrianofx@gmail.com>
2025-02-20 00:00:54 -0300

    Estrutura HTML

--- index.php
+++ index.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    
+
     <title>Genenciador de Filmes - ab filmes</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <header>
+        <nav></nav>
+    </header>
+
+    <main>
+    </main>
 </body>
 </html>
