340cc254f1057aa2bd839d9ab96a855ddec36c34
Gabriel Adriano <gabrieladrianofx@gmail.com>
2025-02-23 15:51:25 -0300

    Criando dados

---
+++ dados.php
@@ -0,0 +1,86 @@
+<?php
+
+$media_movies = [
+    [
+        "title" => "Pobres Criaturas",
+        "genre" => "Drama",
+        "year" => "2023",
+        "indication" => "4,5",
+        'images' => [
+            'src' => '/public/assets/img-movie/pobres-criaturas.png',
+            'alt' => 'Img Pobres Criaturas'
+        ]
+    ],
+    [
+        "title" => "Meu Malvado Favorito 4",
+        "genre" => "Animação",
+        "year" => "2024",
+        "indication" => "4,5",
+        'images' => [
+            'src' => '/public/assets/img-movie/meu-malvado-favorito-4.png',
+            'alt' => 'Img Meu Malvado Favorito 4'
+        ]
+    ],
+    [
+        "title" => "Deadpool & Wolverine",
+        "genre" => "Ação",
+        "year" => "2023",
+        "indication" => "4,5",
+        'images' => [
+            'src' => '/public/assets/img-movie/deadpool-wolverine.png',
+            'alt' => 'Img Deadpool & Wolverine'
+        ]
+    ],
+    [
+        "title" => "O Corvo",
+        "genre" => "Fantasia",
+        "year" => "2024",
+        "indication" => "4,5",
+        'images' => [
+            'src' => '/public/assets/img-movie/o-corvo.png',
+            'alt' => 'Img O Corvo'
+        ]
+    ],
+    [
+        "title" => "O Senhor dos Anéis: A Sociedade do Anel",
+        "genre" => "Aventura",
+        "year" => "2001",
+        "indication" => "4,5",
+        'images' => [
+            'src' => '/public/assets/img-movie/senhor-dos-aneis-a-sociedade-do-anel.png',
+            'alt' => 'Img O Senhor dos Anéis: A Sociedade do Anel'
+        ]
+    ],
+    [
+        "title" => "Alien: Covenant",
+        "genre" => "Horror",
+        "year" => "2017",
+        "indication" => "4,5",
+        'images' => [
+            'src' => '/public/assets/img-movie/alien-covenant.png',
+            'alt' => 'Img Alien: Covenant'
+        ]
+    ],
+    [
+        "title" => "Divertidamente 2",
+        "genre" => "Animação",
+        "year" => "2024",
+        "indication" => "4,5",
+        'images' => [
+            'src' => '/public/assets/img-movie/divertidamente-2.png',
+            'alt' => 'Img Divertidamente 2'
+        ]
+    ],
+    [
+        "title" => "Mad Max: Estrada da Fúria",
+        "genre" => "Ação",
+        "year" => "2015",
+        "indication" => "4,5",
+        'images' => [
+            'src' => '/public/assets/img-movie/mad-max-estrada-da-furia.png',
+            'alt' => 'Img Mad Max: Estrada da Fúria'
+        ]
+    ],
+]
+
+?>
